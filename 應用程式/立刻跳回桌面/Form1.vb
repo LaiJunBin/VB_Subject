789b72f4afe003f8a88cf0ee55d1147178959679
xyz607xx <xyz607xx@gmail.com>
2017-12-12 15:47:24 +0800

    切回桌面

---
+++ 應用程式/立刻跳回桌面/Form1.vb
@@ -0,0 +1,9 @@
+﻿Public Class Form1
+    Private Sub Form1_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+        Dim clsidShell As New Guid("13709620-C279-11CE-A49E-444553540000")
+        Dim shell1 As Object = Activator.CreateInstance( _
+        Type.GetTypeFromCLSID(clsidShell))
+        shell1.ToggleDesktop()
+        Close()
+    End Sub
+End Class
