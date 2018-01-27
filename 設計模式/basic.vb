d373fea07c0f850a30f53e45ab97b8b9b7018f50
xyz607xx <xyz607xx@gmail.com>
2018-01-27 22:15:28 +0800

    存放常用函數方法及副程式

---
+++ 設計模式/basic.vb
@@ -0,0 +1,9 @@
+﻿Module basic
+    Sub print(ByVal ParamArray data())
+        Console.WriteLine(Strings.Join(data, " "))
+    End Sub
+    Sub pause()
+        Console.WriteLine("按下任意鍵結束...")
+        Console.ReadKey()
+    End Sub
+End Module
