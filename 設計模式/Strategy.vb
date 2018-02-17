f0f8040ed6f1f38678d4249aff8bf90a28b5e87a
xyz607xx <xyz607xx@gmail.com>
2018-02-17 19:06:50 +0800

    策略模式

---
+++ 設計模式/Strategy.vb
@@ -0,0 +1,44 @@
+﻿Module Strategy
+    Interface FlightStrategy
+        Sub execute()
+    End Interface
+    Class NormalAttack
+        Implements FlightStrategy
+        Public Sub execute() Implements FlightStrategy.execute
+            print("使用普通攻擊!")
+        End Sub
+    End Class
+    Class UseSkill
+        Implements FlightStrategy
+        Public Sub execute() Implements FlightStrategy.execute
+            print("使用技能!")
+        End Sub
+    End Class
+    Class UseItem
+        Implements FlightStrategy
+        Public Sub execute() Implements FlightStrategy.execute
+            print("使用道具!")
+        End Sub
+    End Class
+    Class Adventurer
+        Private flightStrategy As FlightStrategy
+        Sub attack()
+            If Me.flightStrategy Is Nothing Then
+                flightStrategy = New NormalAttack
+            End If
+            flightStrategy.execute()
+        End Sub
+        Sub choiceStrategy(ByVal strategy As FlightStrategy)
+            Me.flightStrategy = strategy
+        End Sub
+    End Class
+    Sub Main()
+        Dim player As New Adventurer
+        player.attack()
+        player.choiceStrategy(New UseItem)
+        player.attack()
+        player.choiceStrategy(New UseSkill)
+        player.attack()
+        pause()
+    End Sub
+End Module
