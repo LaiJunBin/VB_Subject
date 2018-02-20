1fea268db93cac8bf5cd0611795ddd3e33642b6c
xyz607xx <xyz607xx@gmail.com>
2018-02-20 20:21:32 +0800

    轉接器模式

---
+++ 設計模式/Adapter.vb
@@ -0,0 +1,31 @@
+﻿Module Adapter
+    Public Interface Archer
+        Sub Shot()
+    End Interface
+    Public Interface Wizard
+        Sub FireBall()
+    End Interface
+    Public Class ArcherPlayer
+        Implements Archer
+        Public Sub Shot() Implements Archer.Shot
+            print("射箭")
+        End Sub
+    End Class
+    Public Class Adapter
+        Implements Wizard
+        Private Archer As Archer
+        Sub New(ByVal archer As Archer)
+            Me.Archer = archer
+        End Sub
+        Public Sub FireBall() Implements Wizard.FireBall
+            print("在弓箭上點火")
+            Archer.Shot()
+            print("火球飛出去了!")
+        End Sub
+    End Class
+    Sub Main()
+        Dim player As New Adapter(New ArcherPlayer())
+        player.FireBall()
+        pause()
+    End Sub
+End Module
