33cbe7ecb999dd85b4dff4b2c299924311fabc26
xyz607xx <xyz607xx@gmail.com>
2018-08-26 23:49:43 +0800

    運算式抽象類別

---
+++ 應用程式/學科測驗系統(第二版)/IExpression.vb
@@ -0,0 +1,7 @@
+﻿Imports System.Text
+Public MustInherit Class IExpression
+    Inherits Expression
+    Protected maxDepth As Integer = 3
+    Public MustOverride Sub process(ByRef funcName As String, ByRef funcParams() As String, ByRef source As StringBuilder, ByRef target As String, Optional ByVal useReturn As Boolean = True)
+    Public MustOverride Sub _process(ByRef source As StringBuilder, ByVal var As List(Of String), ByVal maxDepth As Integer, Optional ByVal depth As Integer = 0, Optional ByVal tabCount As Integer = 1)
+End Class
